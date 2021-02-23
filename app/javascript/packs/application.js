6a392990923692510276ec1349b683aaea702581
Demétrius Mânica <demeteixeira@hotmail.com>
2021-02-22 22:23:32 -0300

    Removed test folder and admin-lte

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
-require('admin-lte');
 
 import 'bootstrap'
 import '../stylesheets/application'
