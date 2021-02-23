a2e2c880190dff2540e026f3f35d9c31afb45912
Yago Kades <yago.cercal@arcasoltec.com.br>
2021-02-22 23:10:01 -0300

    added files

---
+++ spec/views/employees/edit.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "employees/edit", type: :view do
+  before(:each) do
+    @employee = assign(:employee, Employee.create!(
+      name: "MyString",
+      cpf: "MyString",
+      email: "MyString"
+    ))
+  end
+
+  it "renders the edit employee form" do
+    render
+
+    assert_select "form[action=?][method=?]", employee_path(@employee), "post" do
+
+      assert_select "input[name=?]", "employee[name]"
+
+      assert_select "input[name=?]", "employee[cpf]"
+
+      assert_select "input[name=?]", "employee[email]"
+    end
+  end
+end
---
+++ spec/views/employees/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "employees/index", type: :view do
+  before(:each) do
+    assign(:employees, [
+      Employee.create!(
+        name: "Name",
+        cpf: "Cpf",
+        email: "Email"
+      ),
+      Employee.create!(
+        name: "Name",
+        cpf: "Cpf",
+        email: "Email"
+      )
+    ])
+  end
+
+  it "renders a list of employees" do
+    render
+    assert_select "tr>td", text: "Name".to_s, count: 2
+    assert_select "tr>td", text: "Cpf".to_s, count: 2
+    assert_select "tr>td", text: "Email".to_s, count: 2
+  end
+end
