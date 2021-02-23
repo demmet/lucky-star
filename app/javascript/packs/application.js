524892f749a9df444f9a4437205e0c4899b7756f
Demétrius Mânica <demeteixeira@hotmail.com>
2021-02-22 23:26:10 -0300

    Added admin-lte files and imports

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require('@rails/ujs').start()
 require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
+require('admin-lte')
 
 import 'bootstrap'
 import '../stylesheets/application'
